6194b2902cd08208ad961dd0f0163e55c0d187b8
Andy Chuong <andy.chuong@colorado.edu>
2018-09-07 15:19:44 -0600

    up arrow

--- src/app.js
+++ src/app.js
@@ -31,6 +31,8 @@ document.addEventListener('DOMContentLoaded', function(event) {
           }
           if(j !== 0 && i !== 0){
             td.id = `${alpha[j-1]}${i}`
+            td.setAttribute('row', i)
+            td.setAttribute('col',alpha[j-1])
           }
           tr.appendChild(td)
         }
@@ -40,19 +42,45 @@ document.addEventListener('DOMContentLoaded', function(event) {
   }
   createGrid()
   // Add click to select cell
+  if(!firstSel){
+    document.getElementById('A1').classList.add('selected')
+    firstSel = true
+  }
+  let cur = document.getElementsByClassName('selected')[0]
   document.addEventListener('click', (e) => {
     let theGrid = document.getElementById('mainGrid')
     if (e.target.className !== 'header' && e.target.className === 'cell'){
       // e.target.focus()
-      if(!firstSel){
-        e.target.classList.add('selected')
-        firstSel = true
-      } else {
-        document.getElementsByClassName('selected')[0].classList.remove('selected')
-        e.target.classList.add('selected')
-      }
+      document.getElementsByClassName('selected')[0].classList.remove('selected')
+      e.target.classList.add('selected')
       // e.target.style.backgroundColor = "black"
-
+      cur = document.getElementsByClassName('selected')[0]
     }
   })
+
+  // Arrow Keys
+  document.onkeydown = checkKey;
+
+  function checkKey(e) {
+      e = e || window.event;
+      console.log(cur.id)
+      if (e.keyCode == '38') {
+        // up arrow
+        let upCol = `${cur.getAttribute('col')}${parseInt(cur.getAttribute('row'))-1}`
+        console.log(upCol)
+        document.getElementsByClassName('selected')[0].classList.remove('selected')
+        document.getElementById(upCol).classList.add('selected')
+        cur = document.getElementsByClassName('selected')[0]
+      }
+      else if (e.keyCode == '40') {
+          // down arrow
+      }
+      else if (e.keyCode == '37') {
+         // left arrow
+      }
+      else if (e.keyCode == '39') {
+         // right arrow
+      }
+
+  }
 })
