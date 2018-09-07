8bc3d6cc41f2adc56a860b1c09424b6979a530b5
Andy Chuong <andy.chuong@colorado.edu>
2018-09-07 15:33:50 -0600

    all arrow functionality

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', function(event) {
   let alphaStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
   let alpha = alphaStr.split('')
   let firstSel = false
+  let rows = 31
+  let cols = 27
   // Create grid
   let createGrid = () => {
     let grid = document.createElement('table')
@@ -11,14 +13,16 @@ document.addEventListener('DOMContentLoaded', function(event) {
     // grid.style.width = '100%'
     // grid.setAttribute('border', '1')
     // var tbdy = document.createElement('tbody');
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < rows; i++) { // Rows
         let tr = document.createElement('tr')
-        for (let j = 0; j < 40; j++) {
+        for (let j = 0; j < cols; j++) { // Cols
           let td = document.createElement('td')
+          // Add class 'cell'
           td.classList.add('cell')
           if(i === 0 && j === 0){
             td.classList.add('header')
           }
+          // Add class 'header' fill values
           if (i === 0 && j !== 0){
             td.innerText = alpha[j-1]
             td.classList.add('header')
@@ -29,16 +33,17 @@ document.addEventListener('DOMContentLoaded', function(event) {
             td.classList.add('header')
             td.id = i
           }
+          // Add ids and custom attributes
           if(j !== 0 && i !== 0){
             td.id = `${alpha[j-1]}${i}`
             td.setAttribute('row', i)
             td.setAttribute('col',alpha[j-1])
           }
-          tr.appendChild(td)
+          tr.appendChild(td) // add cell to row
         }
-        grid.appendChild(tr);
+        grid.appendChild(tr); //add row to table
     }
-    body.appendChild(grid)
+    body.appendChild(grid) // add table to body
   }
   createGrid()
   // Add click to select cell
@@ -64,22 +69,43 @@ document.addEventListener('DOMContentLoaded', function(event) {
   function checkKey(e) {
       e = e || window.event;
       console.log(cur.id)
+      let colIndex = alpha.indexOf(cur.getAttribute('col'))
       if (e.keyCode == '38') {
         // up arrow
-        let upCol = `${cur.getAttribute('col')}${parseInt(cur.getAttribute('row'))-1}`
-        console.log(upCol)
-        document.getElementsByClassName('selected')[0].classList.remove('selected')
-        document.getElementById(upCol).classList.add('selected')
-        cur = document.getElementsByClassName('selected')[0]
+        if(parseInt(cur.getAttribute('row'))>1){
+          let upCol = `${cur.getAttribute('col')}${parseInt(cur.getAttribute('row'))-1}`
+          console.log(upCol)
+          document.getElementsByClassName('selected')[0].classList.remove('selected')
+          document.getElementById(upCol).classList.add('selected')
+          cur = document.getElementsByClassName('selected')[0]
+        }
       }
       else if (e.keyCode == '40') {
           // down arrow
+          if(parseInt(cur.getAttribute('row'))<rows-1){
+            let downCol = `${cur.getAttribute('col')}${parseInt(cur.getAttribute('row'))+1}`
+            document.getElementsByClassName('selected')[0].classList.remove('selected')
+            document.getElementById(downCol).classList.add('selected')
+            cur = document.getElementsByClassName('selected')[0]
+          }
       }
       else if (e.keyCode == '37') {
          // left arrow
+         if(colIndex> 0){
+           let leftCol = `${alpha[colIndex-1]}${cur.getAttribute('row')}`
+           document.getElementsByClassName('selected')[0].classList.remove('selected')
+           document.getElementById(leftCol).classList.add('selected')
+           cur = document.getElementsByClassName('selected')[0]
+         }
       }
       else if (e.keyCode == '39') {
          // right arrow
+         if(colIndex < cols -2){
+           let rightCol = `${alpha[colIndex+1]}${cur.getAttribute('row')}`
+           document.getElementsByClassName('selected')[0].classList.remove('selected')
+           document.getElementById(rightCol).classList.add('selected')
+           cur = document.getElementsByClassName('selected')[0]
+         }
       }
 
   }
