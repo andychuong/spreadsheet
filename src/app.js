4edbc23b023a6ebe563060e017635751c08e7fcc
Andy Chuong <andy.chuong@colorado.edu>
2018-09-07 14:56:44 -0600

    add id to each cell, create selected class

--- src/app.js
+++ src/app.js
@@ -1,34 +1,58 @@
 document.addEventListener('DOMContentLoaded', function(event) {
   // Define Body
   const body = document.body
-  var alphaStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";;
-  var alpha = alphaStr.split('');
-  // Create table
+  let alphaStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+  let alpha = alphaStr.split('')
+  let firstSel = false
+  // Create grid
   let createGrid = () => {
     let grid = document.createElement('table')
-
-  }
-  function tableCreate() {
-    let body = document.getElementsByTagName('body')[0];
-    let tbl = document.createElement('table');
-    tbl.style.width = '100%';
-    tbl.setAttribute('border', '1');
+    grid.id = "mainGrid"
+    // grid.style.width = '100%'
+    // grid.setAttribute('border', '1')
     // var tbdy = document.createElement('tbody');
-    for (let i = 0; i < 21; i++) {
-        let tr = document.createElement('tr');
-        for (let j = 0; j < 27; j++) {
+    for (let i = 0; i < 50; i++) {
+        let tr = document.createElement('tr')
+        for (let j = 0; j < 40; j++) {
           let td = document.createElement('td')
+          td.classList.add('cell')
+          if(i === 0 && j === 0){
+            td.classList.add('header')
+          }
           if (i === 0 && j !== 0){
             td.innerText = alpha[j-1]
+            td.classList.add('header')
+            td.id = alpha[j-1]
           }
           if (j === 0 && i > 0){
             td.innerText = i
+            td.classList.add('header')
+            td.id = i
+          }
+          if(j !== 0 && i !== 0){
+            td.id = `${alpha[j-1]}${i}`
           }
           tr.appendChild(td)
         }
-        tbl.appendChild(tr);
+        grid.appendChild(tr);
     }
-    body.appendChild(tbl)
+    body.appendChild(grid)
   }
-  tableCreate()
+  createGrid()
+  // Add click to select cell
+  document.addEventListener('click', (e) => {
+    let theGrid = document.getElementById('mainGrid')
+    if (e.target.className !== 'header' && e.target.className === 'cell'){
+      // e.target.focus()
+      if(!firstSel){
+        e.target.classList.add('selected')
+        firstSel = true
+      } else {
+        document.getElementsByClassName('selected')[0].classList.remove('selected')
+        e.target.classList.add('selected')
+      }
+      // e.target.style.backgroundColor = "black"
+
+    }
+  })
 })
