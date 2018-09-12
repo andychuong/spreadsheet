59969682daa3270f362583e288675b5c6abf2980
Andy Chuong <andy.chuong@colorado.edu>
2018-09-12 16:34:35 -0600

    all edge test cases for arrow keys

--- test/table.test.js
+++ test/table.test.js
@@ -75,6 +75,39 @@ describe("createTable", function () {
      expect(rows[1].children[1].classList.contains('selected')).to.eq(false)
    })
 
+   it("Check up edge case", function () {
+
+     // For the setup in this test, check if the left arrow key will move beyond col A
+     let tbody = table.querySelector('tbody')
+     let rows = tbody.querySelectorAll('tr')
+     let A1 = rows[1].children[1]
+
+     // Simulate clicking left key down
+     let event = new KeyboardEvent('keydown', {bubbles: true, keyCode: '38'})
+     document.dispatchEvent(event)
+
+     // Check to make sure that the A1 cell is still selected
+     expect(rows[1].children[1].classList.contains('selected')).to.eq(true)
+   })
+
+   it("Check down edge case", function () {
+
+     // For the setup in this test, check if the left arrow key will move beyond col A
+     let tbody = table.querySelector('tbody')
+     let rows = tbody.querySelectorAll('tr')
+     let A30 = rows[30].children[1]
+
+     // Select A30
+     A30.dispatchEvent(new Event('click', {bubbles: true}))
+     // Simulate clicking down key down
+     let event = new KeyboardEvent('keydown', {bubbles: true, keyCode: '40'})
+     document.dispatchEvent(event)
+
+     // Check to see if A30 is still selected
+     expect(A30.classList.contains('selected')).to.eq(true)
+
+   })
+
    it("Check left edge case", function () {
 
      // For the setup in this test, check if the left arrow key will move beyond col A
@@ -89,5 +122,24 @@ describe("createTable", function () {
      // Check to make sure that the A1 cell is still selected
      expect(rows[1].children[1].classList.contains('selected')).to.eq(true)
    })
+
+   it("Check right edge case", function () {
+
+     // For the setup in this test, check if the left arrow key will move beyond col A
+     let tbody = table.querySelector('tbody')
+     let rows = tbody.querySelectorAll('tr')
+     let Z30 = rows[30].children[26]
+
+     // Select A30
+     Z30.dispatchEvent(new Event('click', {bubbles: true}))
+     // Simulate clicking down key down
+     let event = new KeyboardEvent('keydown', {bubbles: true, keyCode: '39'})
+     document.dispatchEvent(event)
+
+     // Check to see if A30 is still selected
+     expect(Z30.classList.contains('selected')).to.eq(true)
+
+   })
+
  })
 })
